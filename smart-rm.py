d6ed00b3629ba279effab0e3cf49e6a5cd525398
francescobianco <info.francescobianco@gmail.com>
2023-11-18 08:44:56 +0100

    First commit

--- smart-rm.py
+++ smart-rm.py
@@ -2,10 +2,20 @@
 import uvicorn
 
 async def app(scope, receive, send):
-    return "test"
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ]
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, world!',
+    })
 
 
 if __name__ == "__main__":
-    config = uvicorn.Config("smart-rm:app", port=5005, log_level="info")
+    config = uvicorn.Config("smart-rm:app", port=5005, host="0.0.0.0", log_level="info")
     server = uvicorn.Server(config)
     server.run()
